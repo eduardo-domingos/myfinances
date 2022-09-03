4d480fdf01f14f8df43a1d66ac5d896cf0b3ec57
Eduardo Domingos <eduardoaparecido461@gmail.com>
2022-09-02 21:17:19 -0300

    Adiciona função para criar o diretório de logs, caso não exista

--- App/debug/phpError.php
+++ App/debug/phpError.php
@@ -1,6 +1,17 @@
 <?php
 
 /**
+ * Função para criar o diretório de logs, caso não exista
+ * @param string $pathDir
+ */
+function createDirLogs(string $pathDir): void
+{
+    if(!file_exists($pathDir)){
+        mkdir($pathDir);
+    }
+}
+
+/**
  * Função para registrar erros e avisos no log
  * @param int $error
  * @param string $message
@@ -10,15 +21,18 @@
  */
 function phpError(int $error, string $message, string $file, int $line): void
 {
-        if(DEBUG){
-            $logs = "[".date('d/m/Y H:i:s')."] Erro: {$message} no arquivo {$file} na linha {$line}\n\n";
+    if(DEBUG){
 
-            error_log($logs, 3, __DIR__.'/../logs/phpErro.log');
+        createDirLogs(__DIR__.'/../logs');
 
-            if($error == 1 || $error == 256){
-                die();
-            }
+        $logs = "[".date('d/m/Y H:i:s')."] Erro: {$message} no arquivo {$file} na linha {$line}\n\n";
+
+        error_log($logs, 3, __DIR__.'/../logs/phpErro.log');
+
+        if($error == 1 || $error == 256){
+            die();
         }
+    }
 }
 
 set_error_handler('phpError');
